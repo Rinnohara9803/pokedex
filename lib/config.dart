21e35a0577d25175761630dff6163d51155265b5
Rinnohara9803 <sagarprajapati9803@gmail.com>
2023-08-12 13:42:20 +0545

    added config file

---
+++ lib/config.dart
@@ -0,0 +1,1 @@
+const String apiUrl = 'https://pokeapi.co/api/v2/pokemon?offset=20&limit=20';
--- lib/data/data-providers/pokemon_api.dart
+++ lib/data/data-providers/pokemon_api.dart
@@ -3,10 +3,12 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:pokedex/presentation/resources/strings_manager.dart';
 
+import '../../config.dart';
+
 class PokemonApi {
   Future<dynamic> fetchListOfPokemons() async {
     final response = await http.get(
-      Uri.parse('https://pokeapi.co/api/v2/pokemon?offset=20&limit=20'),
+      Uri.parse(apiUrl),
     );
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
